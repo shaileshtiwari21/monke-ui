2ff405a5d2ea71238023bc81ef0adda2963726c1
shaileshtiwari21 <saffron.shaileshtiwari@gmail.com>
2023-07-28 18:20:02 +0530

    ready to go

--- src/app/layout/Navbar.js
+++ src/app/layout/Navbar.js
@@ -6,7 +6,9 @@ import { AiOutlineHome } from "react-icons/ai";
 import { IoIosArrowUp } from "react-icons/io";
 import NavIcon from "../assets/Navicon.png";
 import Group from "../assets/group.png";
-
+import Arrow from "../assets/arrow.png"
+import Trofee from "../assets/trofee.png"
+import Home from "../assets/Home.png"
 import { useState } from "react";
 import Image from "next/image";
 
@@ -41,7 +43,7 @@ const Navbar = () => {
           <div className="relative hidden sm:block">
             <button
               type="button"
-              className="text-[#184623] bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-md px-4 py-3 text-center mr-3 md:mr-0 dark:focus:ring-blue-800"
+              className="text-[#184623] bg-[#FFFFFF]  focus:outline-none font-medium text-md px-4 py-3 text-center mr-3 md:mr-0 "
               onClick={() => setShowDropdown(!showDropdown)}
             >
               <span className="flex">
@@ -52,61 +54,64 @@ const Navbar = () => {
             </button>
 
             {showDropdown && (
-              <div className="z-10 font-normal bg-white divide-y divide-gray-100  shadow   dark:divide-gray-600 absolute top-0 right-0 mt-14 ">
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
-                  aria-labelledby="dropdownLargeButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-[#184623] dark:hover:text-emerald-600"
-                    >
-                      {" "}
-                      <span className="flex">
-                        <AiOutlineHome className="mr-2 h-[20px] " />
-                        Dashboard
-                      </span>{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2   text-[#184623] dark:hover:text-emerald-600"
-                    >
-                      <span className="flex">
-                        <AiOutlineHome className="mr-2 h-[20px]" />
-                        User Dashboard
-                      </span>{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-[#184623] dark:hover:text-emerald-600"
-                    >
-                      {" "}
-                      <span className="flex">
-                        <AiOutlineHome className="mr-2 h-[20px] " />
-                        Disconnect
-                      </span>{" "}
-                    </a>
-                  </li>
-                </ul>
-                <div className="flex justify-center pt-8 px-8 pb-2">
-                  <Image src={Monke} alt="" className="h-10 w-10" />
-                </div>
-                <div className="flex justify-center px-4 pb-4">
-                  <Image src={Group} alt="" className=" h-6" />
-                </div>
-              </div>
+          <div className="z-10 font-normal bg-white divide-y divide-gray-100  shadow   dark:divide-gray-600 absolute top-0 right-0 mt-[75px] ">
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-400"
+            aria-labelledby="dropdownLargeButton"
+          >
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-[#184623] dark:hover:text-emerald-600"
+              >
+                {" "}
+                <span className="flex gap-2">
+                <Image  src={Home} />
+                 
+                  Dashboard
+                </span>{" "}
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2   text-[#184623] dark:hover:text-emerald-600"
+              >
+                <span className="flex gap-2">
+                <Image  src={Trofee} />
+                 
+                  User Dashboard
+                </span>{" "}
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 text-[#184623] dark:hover:text-emerald-600"
+              >
+                {" "}
+                <span className="flex gap-2">
+                <Image  src={Arrow} className="h-4" />
+                  Disconnect
+                </span>{" "}
+              </a>
+            </li>
+          </ul>
+          <div className="flex justify-center pt-8 px-8 pb-2">
+            <Image src={Monke} alt="" className="h-10 w-10" />
+          </div>
+          <div className="flex justify-center px-4 pb-4">
+            <Image src={Group} alt="" className=" h-6" />
+          </div>
+        </div>
             )}
           </div>
 
-          <button
+          <button 
+             onClick={() => setShowDropdown(!showDropdown)}
             data-collapse-toggle="navbar-sticky"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none "
             aria-controls="navbar-sticky"
             aria-expanded="false"
           >
@@ -128,6 +133,59 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
+        
+        {showDropdown && (
+              <div className="z-10 font-normal bg-white divide-y divide-gray-100  shadow   dark:divide-gray-600 absolute top-0 right-0 mt-[75px] sm:hidden">
+                <ul
+                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-[#184623] dark:hover:text-emerald-600"
+                    >
+                      {" "}
+                      <span className="flex gap-2">
+                      <Image  src={Home} className="h-6"/>
+                       
+                        Dashboard
+                      </span>{" "}
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2   text-[#184623] dark:hover:text-emerald-600"
+                    >
+                      <span className="flex gap-2">
+                      <Image  src={Trofee} />
+                       
+                        User Dashboard
+                      </span>{" "}
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-[#184623] dark:hover:text-emerald-600"
+                    >
+                      {" "}
+                      <span className="flex gap-2">
+                      <Image  src={Arrow} className="h-4" />
+                        Disconnect
+                      </span>{" "}
+                    </a>
+                  </li>
+                </ul>
+                <div className="flex justify-center pt-8 px-8 pb-2">
+                  <Image src={Monke} alt="" className="h-10 w-10" />
+                </div>
+                <div className="flex justify-center px-4 pb-4">
+                  <Image src={Group} alt="" className=" h-6" />
+                </div>
+              </div>
+            )}
         <Image src={Monke} alt="" className="h-14 lg:ml-48 hidden sm:block" />
       </div>
     </nav>
